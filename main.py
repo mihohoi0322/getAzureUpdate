37685d51f98783726f9f81428f905e0249b5b235
mihohoi <mihohoi0322@gmail.com>
2022-07-17 15:51:18 +0900

    get RSS

--- main.py
+++ main.py
@@ -2,21 +2,27 @@ import urllib.parse
 from bs4 import BeautifulSoup
 import requests
 import feedparser
-import pprint
-
-category = {"category": "containers"}
-html = "https://azure.microsoft.com/ja-jp/updates/feed" + "?" + urllib.parse.urlencode(category)
-artuckes = feedparser.parse(html)
 
 
-for rss in artuckes.entries:
-    dic = {
-        "title": rss.title,
-        "link": rss.link,
-        "summary": rss.summary,
-        "year": rss.published_parsed.tm_year,
-        "month": rss.published_parsed.tm_mon,
-        "day": rss.published_parsed.tm_mday
-    }
-    print(dic)
+def getRSS(category):
+    categories = {"category": category}
+    html = "https://azure.microsoft.com/ja-jp/updates/feed" + "?" + urllib.parse.urlencode(categories)
+    lists = feedparser.parse(html)
+
+    rss_list = []
+    for rss in lists.entries:
+        dic = {
+            "title": rss.title,
+            "link": rss.link,
+            "summary": rss.summary,
+            "year": rss.published_parsed.tm_year,
+            "month": rss.published_parsed.tm_mon,
+            "day": rss.published_parsed.tm_mday
+        }
+        rss_list.append(dic)
+        print(rss_list)
+    return rss_list
+
+print(getRSS("container"))
+
 
