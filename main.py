855cf9f56fc2889b862346e7dce7186e411fd7bb
mihohoi <mihohoi0322@gmail.com>
2022-07-17 16:14:25 +0900

    get RSS

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import urllib.parse
-from bs4 import BeautifulSoup
-import requests
 import feedparser
 
 
-def getRSS(category):
+def getrss(category):
     categories = {"category": category}
     html = "https://azure.microsoft.com/ja-jp/updates/feed" + "?" + urllib.parse.urlencode(categories)
     lists = feedparser.parse(html)
@@ -20,9 +18,9 @@ def getRSS(category):
             "day": rss.published_parsed.tm_mday
         }
         rss_list.append(dic)
-        print(rss_list)
     return rss_list
 
-print(getRSS("container"))
+print(getrss("containers"))
+print(type(getrss("containers")))
 
 
--- ress.py
+++ ress.py
@@ -1,6 +1,22 @@
-import lxml.etree
+import urllib.parse
+import feedparser
 
-rss = lxml.etree.parse("https://azurecomcdn.azureedge.net/ja-jp/updates/feed/")
-root = rss.getroot()
 
+categories = {"category": "containers"}
+html = "https://azure.microsoft.com/ja-jp/updates/feed" + "?" + urllib.parse.urlencode(categories)
+lists = feedparser.parse(html)
+
+rss_list = []
+
+for rss in lists.entries:
+    dic = {
+
+        "title": rss.title,
+        "link": rss.link,
+        "summary": rss.summary,
+        "year": rss.published_parsed.tm_year,
+        "month": rss.published_parsed.tm_mon,
+        "day": rss.published_parsed.tm_mday
+    }
+    print(dic)
 
