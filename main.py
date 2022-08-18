4d833dfd33bd951dfa80077d2916c99cff18c5a2
miho <mihohoi0322@gmail.com>
2022-08-18 19:01:09 +0900

    pandas add

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import urllib.parse
 import feedparser
+import pandas as pd
 
 # TODO: list a category
 def getrss(category):
     categories = {"category": category}
     html = "https://azure.microsoft.com/ja-jp/updates/feed" + "?" + urllib.parse.urlencode(categories)
     lists = feedparser.parse(html)
+
+    file = open("test.csv", "w")
 
     rss_list = []
     for rss in lists.entries:
@@ -18,12 +21,17 @@ def getrss(category):
             "day": rss.published_parsed.tm_mday
         }
         rss_list.append(dic)
+
     return rss_list
+# TODO: output a csv file use pandas
 
-# TODO: output a csv file
+# TODO: filter to year & month
+def filterMonth(month):
+    list(filter(lambda x: rss_list.sort(month = 8)))
+    return
 
 
-print(getrss("containers"))
-print(type(getrss("containers")))
-
-
+rss_list = getrss("containers")
+print(rss_list)
+print(type(rss_list))
+print(pd.json_normalize(rss_list))
